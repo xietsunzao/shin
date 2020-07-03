45e06c3e61f7c0c5ec6d1708f4b597d12d2f14da
xietsunzao <lordshinjo22@gmail.com>
2020-07-03 18:47:03 +0700

    update database

--- Database/db_shin.sql
+++ Database/db_shin.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 01, 2020 at 07:55 PM
+-- Generation Time: Jul 03, 2020 at 01:46 PM
 -- Server version: 10.4.13-MariaDB
 -- PHP Version: 7.2.31
 
@@ -42,11 +42,30 @@ INSERT INTO `category` (`id_category`, `category_name`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `confirm`
+--
+
+CREATE TABLE `confirm` (
+  `id_confirm` int(1) NOT NULL,
+  `confirm_name` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `confirm`
+--
+
+INSERT INTO `confirm` (`id_confirm`, `confirm_name`) VALUES
+(1, 'unconfirmed'),
+(2, 'confirmed');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `orders`
 --
 
 CREATE TABLE `orders` (
-  `id_order` int(3) NOT NULL,
+  `id_orders` int(3) NOT NULL,
   `id_product` int(3) NOT NULL,
   `total_order` int(11) NOT NULL,
   `order_date` date DEFAULT NULL
@@ -56,7 +75,7 @@ CREATE TABLE `orders` (
 -- Dumping data for table `orders`
 --
 
-INSERT INTO `orders` (`id_order`, `id_product`, `total_order`, `order_date`) VALUES
+INSERT INTO `orders` (`id_orders`, `id_product`, `total_order`, `order_date`) VALUES
 (1, 1, 50, '2020-07-02');
 
 -- --------------------------------------------------------
@@ -79,6 +98,27 @@ CREATE TABLE `product` (
 INSERT INTO `product` (`id_product`, `product_name`, `id_category`, `price`) VALUES
 (1, 'Example', 1, 5000);
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sale`
+--
+
+CREATE TABLE `sale` (
+  `id_sale` int(3) NOT NULL,
+  `id_orders` int(3) DEFAULT NULL,
+  `date_sale` date NOT NULL,
+  `id_confirm` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `sale`
+--
+
+INSERT INTO `sale` (`id_sale`, `id_orders`, `date_sale`, `id_confirm`) VALUES
+(1, 1, '2020-07-01', 1),
+(2, 1, '2020-07-08', 2);
+
 --
 -- Indexes for dumped tables
 --
@@ -90,10 +130,16 @@ ALTER TABLE `category`
   ADD PRIMARY KEY (`id_category`);
 
 --
+-- Indexes for table `confirm`
+--
+ALTER TABLE `confirm`
+  ADD PRIMARY KEY (`id_confirm`);
+
+--
 -- Indexes for table `orders`
 --
 ALTER TABLE `orders`
-  ADD PRIMARY KEY (`id_order`),
+  ADD PRIMARY KEY (`id_orders`),
   ADD KEY `id_product` (`id_product`);
 
 --
@@ -102,6 +148,13 @@ ALTER TABLE `orders`
 ALTER TABLE `product`
   ADD PRIMARY KEY (`id_product`),
   ADD KEY `id_category` (`id_category`);
+
+--
+-- Indexes for table `sale`
+--
+ALTER TABLE `sale`
+  ADD PRIMARY KEY (`id_sale`),
+  ADD KEY `id_order` (`id_orders`,`id_confirm`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -114,16 +167,28 @@ ALTER TABLE `category`
   MODIFY `id_category` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
+-- AUTO_INCREMENT for table `confirm`
+--
+ALTER TABLE `confirm`
+  MODIFY `id_confirm` int(1) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
 -- AUTO_INCREMENT for table `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `id_order` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_orders` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `product`
 --
 ALTER TABLE `product`
   MODIFY `id_product` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `sale`
+--
+ALTER TABLE `sale`
+  MODIFY `id_sale` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
