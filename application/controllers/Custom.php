2e3922877e638d7e2e3d3aa3187079958fd2ef43
xietsunzao <lordshinjo22@gmail.com>
2020-07-13 22:10:18 +0700

    added controller custom

---
+++ application/controllers/Custom.php
@@ -0,0 +1,22 @@
+<?php 
+
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Custom extends CI_Controller {
+
+    
+    public function __construct()
+    {
+        parent::__construct();
+        
+    }
+    
+    public function index()
+    {
+        
+    }
+
+}
+
+/* End of file Custom.php */
